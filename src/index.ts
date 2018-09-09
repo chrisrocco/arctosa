9412d4342de0dbcfd42cc74d457946e2b703c6d5
Chris Rocco <chris.rocco7@gmail.com>
2018-09-08 22:51:31 -0500

    runner script

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
-import {MarketDefinition} from "./markets/models/MarketDefinition"
-import {RootModule} from "./root.module"
+import {RootModule} from "./root/root.module"
 import {getConfig} from "./config"
+import {MarketDef} from "./root/types/models";
 const dotenv = require('dotenv')
 
 dotenv.config()
 
-const PubSub = require(`@google-cloud/pubsub`)
-const pubsub = new PubSub()
-
-pubsub.subscription('rdbs-stream').on('message', (event) => console.log(event.data.toString()))
-
 // TODO - load from filepath or stdin
-const mkt: MarketDefinition = {
+const mkt: MarketDef = {
     "uuid": "ssd-market-definition",
-    "query": {
-        "keywords": "SSD for Macbook",
-        "itemFilter": [
-            {
-                "name": "SoldItemsOnly",
-                "value": "true"
-            }
-        ]
-    }
+    "keywords": "SSD for Macbook"
 }
 
 const run = async () => {
     const root = RootModule(getConfig(process.env))
-    const reply = await root.ctrls.eBayCtrl.fetchMarketData(mkt)
-    console.log('reply', reply)
+    const reply = await root.ctrls.ebay.executeJob(mkt)
 }
+
+
+
+
+// Monitor GCloud Pub/Sub Output
+const PubSub = require(`@google-cloud/pubsub`)
+const pubsub = new PubSub()
+
+let cnt = 0
+const sub = pubsub.subscription('rdbs-stream').on('message', msg => {
+    console.log('inbound market data: ', JSON.parse(msg.data.toString()).market_id)
+    msg.ack()
+    cnt++
+    if(cnt >= 5) sub.removeAllListeners()
+})
+
 
 run()
