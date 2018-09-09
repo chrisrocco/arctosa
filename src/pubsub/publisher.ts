d50a3248b372d61b4a57cf6159ba2bfe3765a6b0
Chris Rocco <chris.rocco7@gmail.com>
2018-09-08 22:50:22 -0500

    publisher generalized

--- src/pubsub/publisher.ts
+++ src/pubsub/publisher.ts
@@ -1,13 +1,9 @@
-import {CompletedItems} from "../ebay/models/CompletedItems";
-
 export const getPublisher =
     (pubsub) =>
         (topic) =>
-            (events) =>
-                (items: CompletedItems) =>
+            (message) =>
 
-                    pubsub
-                        .topic(topic)
-                        .publisher()
-                        .publish(Buffer.from(JSON.stringify(items)))
-                        .then(msgID => events.emit('completedItems.published', msgID))
+                pubsub
+                    .topic(topic)
+                    .publisher()
+                    .publish(Buffer.from(JSON.stringify(message)))
