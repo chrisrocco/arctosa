dbb9a1a4d328b15bf865c524bf7fb4aa969f66fd
Chris Rocco <chris.rocco7@gmail.com>
2018-09-08 22:51:11 -0500

    request resolver for web scraping pagination

--- src/root/nextRequest.ts
+++ src/root/nextRequest.ts
@@ -1,13 +1,20 @@
-import {ItemsReq, MarketDef} from "./types/models";
+import {ItemsReq, ItemsRes} from "./types/models";
+import {next} from "./types/functions";
 
-export const seedMarketRequest = (mkt: MarketDef): ItemsReq => ({
-    market_id: mkt.uuid,
-    call_depth: 0,
-    query: {
-        keywords: mkt.keywords,
-        paginationInput: {
-            pageNumber: 1,
-            entriesPerPage: 10
+export const getNextRequest = (maxCallDepth): next => (res: ItemsRes): ItemsReq | null => {
+
+    if(res.call_depth >= maxCallDepth) return null
+    if(res.paginationOutput.pageNumber === res.paginationOutput.totalPages) return null
+
+    return {
+        market_id: res.market_id,
+        call_depth: res.call_depth + 1,
+        query: {
+            ...res.request_query,
+            paginationInput: {
+                pageNumber: res.request_query.paginationInput.pageNumber + 1,
+                entriesPerPage: res.request_query.paginationInput.entriesPerPage
+            }
         }
     }
-})
+}
