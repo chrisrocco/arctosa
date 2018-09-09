0461663e52cca18f1ec63fc9c4276e65959f6bd0
Chris Rocco <chris.rocco7@gmail.com>
2018-09-08 22:50:37 -0500

    abstract data pipeline

--- src/pipelines/pipeline.module.ts
+++ src/pipelines/pipeline.module.ts
@@ -1,23 +1,34 @@
-import {ItemsReq, ItemsRes, MarketDef} from "./types"
 import {Subject} from "rxjs"
-import {callEbay, next, parse, publish, seed} from "./functions"
+import {callEbay, next, parse, publish} from "../root/types/functions"
 import {filter, map, switchMap} from "rxjs/operators"
+import {ItemsReq, ItemsRes} from "../root/types/models";
 
-export const PipelineModule = ({callEbay, parse, next, seed, publish}) => {
+const push = stream => data => stream.next(data)
+
+interface deps {
+    callEbay: callEbay,
+    parse: parse,
+    publish: publish
+    next: next
+}
+
+export const PipelineModule = ({ callEbay, parse, publish, next }: deps) => {
 
     const jobs$ = new Subject<ItemsReq>()
     const items$ = new Subject<ItemsRes>()
 
     // process jobs as they arrive
-    jobs$.pipe(switchMap(callEbay)).subscribe((res: ItemsRes) => items$.next(res))
+    jobs$.pipe(switchMap(callEbay)).subscribe(push(items$))
 
     // publish all responses to the pub/sub network
     items$.pipe(map(parse)).subscribe(publish)
 
     // crawl next pages (pagination)
-    items$.pipe(map(next), filter(x => !!x)).subscribe(req => jobs$.next(req))
+    items$.pipe(map(next), filter(x => !!x)).subscribe(push(jobs$))
 
     return {
-        executeJob: (market: MarketDef) => jobs$.next(seed(market))
+        queueRequest: (req: ItemsReq) => jobs$.next(req),
+        jobs$,
+        items$
     }
 }
